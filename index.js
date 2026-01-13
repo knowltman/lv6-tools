3d6ce76570d8053e35d144880a97b7c04f3b83f0
Ryan Knowlton <ryan@rhinogfx.com>
2026-01-13 12:10:20 -0700

    working on index.js

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// 🔥 STATIC FIRST
-app.use(express.static(path.join(__dirname, "dist")));
-
 // API
 app.use("/api", backend);
+
+// Fix MIME type for Vite ES modules (xcloud issue)
+app.use((req, res, next) => {
+  if (req.path.endsWith(".js")) {
+    res.type("application/javascript");
+  }
+  next();
+});
+
+// Serve Vite build
+app.use(express.static(path.join(__dirname, "dist")));
 
 // SPA fallback LAST
 app.get("*", (req, res) => {
