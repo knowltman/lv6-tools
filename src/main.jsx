65c7668459c8b761ff232bb95b116f413891d26f
Ryan Knowlton <ryan@rhinogfx.com>
2026-03-15 18:57:22 -0600

    just triggering the push

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,4 @@ axios.defaults.withCredentials = true;
 
 // Create a root and render the app using createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>
-);
+root.render(<App />);
